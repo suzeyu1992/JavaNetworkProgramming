b429ed5fb4902b30038f5eb762719ac5b768a0cf
suzeyu <suzeyu_1992@163.com>
2016-10-12 22:36:57 +0800

    对上一此提交通道进行非阻塞的处理

--- src/nio/ChargenClient.java
+++ src/nio/ChargenClient.java
@@ -28,11 +28,30 @@ public class ChargenClient {
             ByteBuffer buffer = ByteBuffer.allocate(74);
             WritableByteChannel out = Channels.newChannel(System.out);
 
+            // 设置连接为非阻塞模式
+            client.configureBlocking(false);
+
+            while (true){
+                // 把每次循环都要运行的代码放在此处, 不管有没有数据
+                int readNum = client.read(buffer);
+                if (readNum > 0){
+                    buffer.flip();
+                    out.write(buffer);
+                    buffer.clear();
+                }else if (readNum == -1){
+                    // 这里不会发生, 除非服务器发生异常
+                    break;
+                }
+            }
+
+            /*
+             // 阻塞模式的读取
             while ( client.read(buffer) != -1){
                 buffer.flip();       // 缓冲区回绕起始读取
                 out.write(buffer);
                 buffer.clear();
             }
+             */
 
         } catch (IOException e) {
             e.printStackTrace();
